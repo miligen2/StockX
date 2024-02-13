3205ed52449ca58934aa7d697272bbb845e7c9a9
Miligen <angelo0204@hotmail.fr>
2024-02-13 15:28:24 +0100

    verification login ok

--- Controleur/connexion.php
+++ Controleur/connexion.php
@@ -1,40 +1,32 @@
 <?php
+include '../API/database.php';
+global $db;
 
-try{
+try {
+    if (isset($_POST['email']) && isset($_POST['password'])) {
+        $email = htmlspecialchars($_POST['email']);
+        $password = $_POST['password'];
 
-    var_dump($_POST);
-if (isset($_POST['email']) && $_POST['password'])
-{
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $sql = "SELECT email FROM utilisateur WHERE email = :email";
-    $stmt = $db->prepare($sql);
-    $stmt->execute(['email' => $email]);
-    $result = $stmt->fetch();
+        $sql = "SELECT email, mot_de_passe FROM utilisateurs WHERE email = :email AND mot_de_passe = :password";
+        $stmt = $db->prepare($sql);
+        $stmt->execute(['email' => $email, 'password' => $password]);
+        $result = $stmt->fetch();
 
-
-    if ($result === true){
-      
-        if ($password == $result['password']){
-            echo "Connexion en cours";
-            header("location: v_dashboard.php");
-            exit();
+        if ($result) {
+            if ($password == $result['mot_de_passe']) {
+                echo "Connexion en cours";
+                header("location: ../Vues/v_dashboard.php");
+                exit();
+            } else {
+                echo "Votre mot de passe est incorrect";
+            }
         } else {
-            echo "Votre email ou mot de passe est incorrect";
+            echo "Votre email est incorrect";
         }
-
     } else {
-        echo "Votre email ou mot de passe est incorrect";
+        echo "Tous les champs ne sont pas remplis";
     }
-
-
-} else {
-    echo "Tous les champs ne sont pas remplis";
-}
 } catch (Exception $e) {
-    echo "Error: ".$e
-
+    echo "Error: " . $e->getMessage();
 }
-
-
 ?>
